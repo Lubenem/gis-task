884033958279ae3b16c510c32e338ad2fb1e4fd6
lchahoub <lchahoub@artelogic.net>
2023-11-07 09:16:41 +0200

    Frontend-Skeleton • Added cors configuration on backend

--- GisTask_Backend_Dotnet/GisTask.Api/Program.cs
+++ GisTask_Backend_Dotnet/GisTask.Api/Program.cs
@@ -5,6 +5,16 @@ using AppContext = GisTask.Domain.AppContext;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+    options.AddDefaultPolicy(
+        policy =>
+            policy.WithOrigins("http://localhost:4200")
+                  .AllowAnyMethod()
+                  .AllowAnyHeader()
+                  .AllowCredentials()
+        )
+    );
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -15,7 +25,6 @@ builder.Services.AddDbContext<AppContext>();
 builder.Services.AddScoped<IDriverService, DriverService>();
 builder.Services.AddScoped<ITripService, TripService>();
 
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,11 +33,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
-
+app.UseCors();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
